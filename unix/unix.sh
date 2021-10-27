70cdbfa0495f633851d1366e9e0e3c298c281436
abdfnx <abdfnabd@gmail.com>
2021-10-27 17:21:26 +0300

    update unix editor installer

--- unix/unix.sh
+++ unix/unix.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-loc=~/.secman/editor
-
 v=$(curl --silent "https://api.github.com/repos/scmn-dev/editor/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 releases_api_url=https://github.com/scmn-dev/editor/releases/download
 smUrl=$releases_api_url/$v/editor
 
-wget -O $loc $smUrl
+wget -P ~/.secman $smUrl
 
-if [ -f $loc ]; then
+if [ -f ~/.secman/editor ]; then
     echo "Secman Editor Installed"
 else
     echo "Secman Editor Installation Failed"
