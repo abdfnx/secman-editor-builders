6eda5ac34e6e335d7b46aff3a165aa40ea018f42
Abdfn <64256993+abdfnx@users.noreply.github.com>
2021-10-27 16:17:18 +0300

    Update unix.sh

--- unix/unix.sh
+++ unix/unix.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
+loc=~/.secman/editor
+
 v=$(curl --silent "https://api.github.com/repos/scmn-dev/editor/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-
 releases_api_url=https://github.com/scmn-dev/editor/releases/download
-
 smUrl=$releases_api_url/$v/editor
 
-wget $smUrl
+wget -O $loc $smUrl
 
-chmod 755 editor
-
-mv editor ~/.secman
-
-if [ -f ~/.secman/editor ]; then
+if [ -f $loc ]; then
     echo "Secman Editor Installed"
 else
     echo "Secman Editor Installation Failed"
