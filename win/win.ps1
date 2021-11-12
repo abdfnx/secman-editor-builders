30f6ead2a55dd9a78c3bd3ceed49be0a978e7410
Abdfn <64256993+abdfnx@users.noreply.github.com>
2021-11-12 20:46:42 +0300

    update windows editor path, add editor checker

--- win/win.ps1
+++ win/win.ps1
@@ -1,16 +1,18 @@
 $release_url = "https://api.github.com/repos/scmn-dev/win-editor/releases"
 $tag = (Invoke-WebRequest -Uri $release_url -UseBasicParsing | ConvertFrom-Json)[0].tag_name
 
-$loc = "$HOME\.secman\editor"
+$loc = "$HOME\.secman\editor.exe"
 
 if (Test-Path -path $loc) {
     Remove-Item $loc -Recurse -Force
 }
 
-Write-Host "Installing secman editor" -ForegroundColor DarkCyan
-
 Invoke-WebRequest https://github.com/scmn-dev/win-editor/releases/download/$tag/editor.exe -outfile editor.exe
 
 Move-Item -Path editor.exe -Destination $HOME\.secman
 
-if 
+if (Test-Path -path $loc) {
+    Write-Host "Secman Editor Installed"
+} else {
+    Write-Host "Secman Editor Installation Failed"
+}
